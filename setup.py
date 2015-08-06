b7c17e17fc05706e09043b042a935f5e47ee49f4
Reuben Cummings <reubano@gmail.com>
2015-08-06 16:00:37 +0300

    Bump to version 0.15.1

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ config = {
     'download_url':
         'https://github.com/reubano/hdxscraper-fts/downloads/hdxscraper-fts*.tgz',
     'author_email': 'reubano@gmail.com',
-    'version': '0.15.0',
+    'version': '0.15.1',
     'install_requires': parse_requirements('requirements.txt'),
     'classifiers': ['Development Status :: 4 - Beta',
         'License :: OSI Approved :: The MIT License (MIT)',
